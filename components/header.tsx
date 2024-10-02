766d5e8bcb4924147cfb8b545e6ad8e7710176dd
Shubham Mazumder <shubham.mazumder@gmail.com>
2024-10-02 01:03:22 -0600

    update github link

--- components/header.tsx
+++ components/header.tsx
@@ -48,7 +48,7 @@ export function Header() {
       <div className="flex items-center justify-end space-x-2">
         <a
           target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          href="https://github.com/cystema/movielang-ai"
           rel="noopener noreferrer"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
